9f74cb9eb7780ccb6be80cb6d37ba449146e638e
Till Studer <11232144+tillstud@users.noreply.github.com>
2020-05-10 20:28:54 +0200

    added basic three.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from "./home-page/home-page.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { NightstandThreejsComponent } from './nightstand-threejs/nightstand-threejs.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component:HomePageComponent },
+  { path: 'nightstand', component:NightstandThreejsComponent },
   // Type additional Path's here
   { path: '404', component:PageNotFoundComponent },
   { path: '**', redirectTo: '/404'}
