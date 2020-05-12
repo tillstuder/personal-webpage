3f6f7f27109e7f65f87fd0471e1e7bfdb8a60841
Till Studer <11232144+tillstud@users.noreply.github.com>
2020-05-12 19:14:26 +0200

    added cylinder

--- src/app/nightstand-threejs/nightstand-threejs.component.ts
+++ src/app/nightstand-threejs/nightstand-threejs.component.ts
@@ -19,18 +19,29 @@ export class NightstandThreejsComponent implements OnInit {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x0000FF , wireframe: true});
-    var cube = new THREE.Mesh(geometry, material);
+    var cube_geometry = new THREE.BoxGeometry(1, 1, 1);
+    var cube_material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
+    var cube = new THREE.Mesh(cube_geometry, cube_material);
     scene.add(cube);
+    //var cone_geometry = new THREE.ConeGeometry(1, 2, 3);
+    //var cone_material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
+    //var cone = new THREE.Mesh(cone_geometry, cone_material);
+    //scene.add(cone);
+    var cylinder_geometry = new THREE.CylinderGeometry(1, 1, 2, 16);
+    var cylinder_material = new THREE.MeshBasicMaterial({ color: 0xff00ff, wireframe: true });
+    var cylinder = new THREE.Mesh(cylinder_geometry, cylinder_material);
+    scene.add(cylinder);
 
     camera.position.z = 5;
 
     var animate = function () {
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      cube.rotation.x += 0.03;
+      cube.rotation.y += 0.03;
+      //cone.rotation.x += 0.01;
+      //cone.rotation.y += 0.01;
+      cylinder.rotation.y += 0.01;
 
       renderer.render(scene, camera);
     };
